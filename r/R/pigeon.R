ed9eb39067e95df4b24deeaeab2755fd6446bc3b
Vallo Varik <varik.mail@gmail.com>
2023-09-18 17:32:25 +0200

    Add pigeon dataset

---
+++ r/R/pigeon.R
@@ -0,0 +1,18 @@
+#' Interorbital width of domestic pigeons
+#'
+#' 40 measurements of interorbital width of a sample of domestic pigeons.
+#' 
+#' @docType data
+#'
+#' @format A data frame with 40 rows and 1 variable:
+#' \tabular{rl}{
+#'  width        \tab Interorbital length in mm \cr
+#' }
+#'
+#' @usage data(pigeon)
+#'
+#' @keywords datasets
+#'
+#' @references Olson, E. C., and R. L. Miller. 1958. Morphological Integration. 
+#'
+"pigeon"
