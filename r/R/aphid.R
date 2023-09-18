8ffa15b9ebc86a1b76b0e6b05f736a8340e1f08a
Vallo Varik <varik.mail@gmail.com>
2023-09-18 17:32:05 +0200

    Fix the doc for aphid

--- r/R/aphid.R
+++ r/R/aphid.R
@@ -5,7 +5,7 @@
 #' 
 #' @docType data
 #'
-#' @format A data frame with 384 rows and 3 variables:
+#' @format A data frame with 25 rows and 1 variable:
 #' \tabular{rl}{
 #'  femur        \tab Femur length in 0.1 mm \cr
 #' }
