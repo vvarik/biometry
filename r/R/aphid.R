6437b764852cdf6942e4f0d0526a5139736514d6
Vallo Varik <varik.mail@gmail.com>
2023-09-18 14:04:56 +0200

    Document the aphid dataset

---
+++ r/R/aphid.R
@@ -0,0 +1,19 @@
+#' Aphid femur lengths
+#'
+#' Twenty-five femur lengths of stem mothers of the aphid _Pemphigus
+#' populitransversus_ in 0.1 mm.
+#' 
+#' @docType data
+#'
+#' @format A data frame with 384 rows and 3 variables:
+#' \tabular{rl}{
+#'  femur        \tab Femur length in 0.1 mm \cr
+#' }
+#'
+#' @usage data(aphid)
+#'
+#' @keywords datasets
+#'
+#' @references Data from R. R. Sokal.
+#'
+"aphid"
