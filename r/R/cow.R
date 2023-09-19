5425c104bfeb16a4d6d48d6397c10b343be2e005
Vallo Varik <varik.mail@gmail.com>
2023-09-19 11:25:56 +0200

    Add cow dataset

---
+++ r/R/cow.R
@@ -0,0 +1,19 @@
+#' Ayrshire cow butterfat content
+#'
+#' Butter fat content in percentages for 120 three-year-old Ayran cows selected
+#' at random from a Canadian stock record book
+#' 
+#' @docType data
+#'
+#' @format A data frame with 120 rows and 1 variable:
+#' \tabular{rl}{
+#'  fat        \tab Interorbital length in mm \cr
+#' }
+#'
+#' @usage data(pigeon)
+#'
+#' @keywords datasets
+#'
+#' @references Sokal & Rohlf Biometry 3rd, p 59.
+#'
+"cow"
