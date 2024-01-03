aae2b5f7d535cf745b5ef952520959b9c74643c1
Vallo Varik <varik.mail@gmail.com>
2024-01-03 11:57:52 +0100

    Add housefly dataset

---
+++ r/R/housefly.R
@@ -0,0 +1,20 @@
+#' Wing lengths of houseflies
+#'
+#' Lengths of 100 wings of houseflies in 0.1 mm
+#' 
+#' @docType data
+#'
+#' @format A data frame with 100 rows and 1 variable:
+#' \tabular{rl}{
+#'  wing_length        \tab Wing length in 0.1 mm \cr
+#' }
+#'
+#' @usage data(houseflies)
+#'
+#' @keywords datasets
+#'
+#' @references Sokal, R. R., and P. E. Hunger. 1955. A morphometric analysis of
+#' DDT-resistant and nonresistant housefly strains. Ann. Entomol. Soc. Amer.
+#' 48:499-507
+#'
+"housefly"
